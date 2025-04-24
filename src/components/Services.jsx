5ed6707848e9284885862ab825fadb3d8d026fca
sruthybabu369 <sruthycb0@gmail.com>
2025-04-24 21:18:34 +0530

    commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/services.css";
+import "../styles/Services.css";
 import { FaCode, FaPaintBrush, FaLaptopCode } from "react-icons/fa";
 
 const Services = () => {
